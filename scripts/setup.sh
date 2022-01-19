ffa951863eb95fe071cc4224f057161d31211e29
lolorol <laurent.lesle@gmail.com>
2022-01-19 04:41:02 +0000

    Add initial website

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,10 @@
 function install {
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
   echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  curl -sL https://deb.nodesource.com/setup_17.x -o nodesource_setup.sh
+  curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
   chmod +x nodesource_setup.sh
-  sudo ./nodesource_setup.sh
+  sudo nodesource_setup.sh
+  rm -rf nodesource_setup.sh
 
   sudo apt-get install -y nodejs yarn
 }
